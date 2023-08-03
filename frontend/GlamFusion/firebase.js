d28158f068f838d10733c69b1af5fa3f2a9d09ec
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2023-08-03 10:49:59 +0200

    initialised firebase

---
+++ frontend/GlamFusion/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAUBfOHo8ta8wul6PK4gI1rJnqQibLotkk",
+  authDomain: "glamfusion.firebaseapp.com",
+  projectId: "glamfusion",
+  storageBucket: "glamfusion.appspot.com",
+  messagingSenderId: "53460564073",
+  appId: "1:53460564073:web:cdd51b3dc0ea271b5d8af8",
+  measurementId: "G-PES8Q8TBM1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
