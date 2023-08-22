23f2cd908b06db52ca0d17ac471a8b01cd36d1da
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2023-08-22 12:13:02 +0200

    ability to toggle between selected services

--- frontend/GlamFusion/src/components/05.ServiceStore/ServiceStore.vue
+++ frontend/GlamFusion/src/components/05.ServiceStore/ServiceStore.vue
@@ -5,7 +5,7 @@ import image2 from '../../assets/img/background-2.webp';
 import image3 from '../../assets/img/background-3.jfif';
 
 const storeInfo = JSON.parse(localStorage.getItem('storeInfo'));
-const {StoreName, StoreLocation, storeImage, StoreMembers, StoreServices} = storeInfo;
+const { StoreName, StoreLocation, storeImage, StoreMembers, StoreServices } = storeInfo;
 
 const baseUrl = ref('http://localhost:1337');
 const changeImageNo = ref(0);
@@ -14,7 +14,9 @@ const bookAppointmentModal = ref(null);
 const calendlyEvent = ref(null);
 const isModalOpen = ref(false);
 const isButtonDisabled = ref(true);
-const bookingBtn = ref('booking-btn-off')
+const bookingBtn = ref('booking-btn-off');
+const infoBtn = ref('infoBtnOn')
+const activeButton = ref(null);
 
 //we will use the selected member to book an appointment.
 //after user clicks the member, we get their booking url from strapi. and make a calendly call using their booking url
@@ -25,14 +27,14 @@ const bookingBtn = ref('booking-btn-off')
 //or else what can happen is that we can get the members of an organization from calendly directly when they are added onto the calendar.
 //this will also allow us to get all members data from one single source of truth e.g get their schedules and all their images and all.
 //then we can take that members name from calendly and then set the name in the db for a booking that will be used in the dashboard
-function getMember(member){
+function getMember(member) {
   bookingBtn.value = 'booking-btn-on';
   isButtonDisabled.value = false;
-  console.log('disabledSTATUS:',  isButtonDisabled.value)
-  console.log(member)
+  console.log('disabledSTATUS:', isButtonDisabled.value);
+  console.log(member);
 }
 
-console.log(StoreName, StoreLocation, storeImage, StoreMembers, StoreServices)
+console.log(StoreName, StoreLocation, storeImage, StoreMembers, StoreServices);
 
 function updateDynamicImage(index, image) {
   changeImageNo.value = index;
@@ -51,11 +53,7 @@ function bookAppointment() {
 
 // Checks if it's a Calendly event
 function isCalendlyEvent(e) {
-  return (
-    e.origin === 'https://calendly.com' &&
-    e.data.event &&
-    e.data.event.indexOf('calendly.') === 0
-  );
+  return e.origin === 'https://calendly.com' && e.data.event && e.data.event.indexOf('calendly.') === 0;
 }
 
 // Listens to Calendly events and sets the event value
@@ -82,76 +80,94 @@ const closeModal = (event) => {
   }
 };
 
+//able to select service to book and toggle between disabling service btns on selection of a service
+const selectService = (index) => {
+  if (activeButton.value === index) {
+    activeButton.value = null;
+  } else {
+    activeButton.value = index;
+  }
+};
 </script>
 
 <template>
-    <main id="service-store-wrapper">
-       <h2 class="storeName">{{ StoreName }}</h2>
-       <h3 class="storeLocation">{{ StoreLocation }}</h3> 
+  <main id="service-store-wrapper">
+    <h2 class="storeName">{{ StoreName }}</h2>
+    <h3 class="storeLocation">{{ StoreLocation }}</h3>
 
-       <section id="store-img-service-display">
-        <transition name="image-slide" mode="out-in">
-            <img :key="dynamicStoreImage" :src="dynamicStoreImage" alt="" class="store-img-service" />
-        </transition>
-       </section>
+    <section id="store-img-service-display">
+      <transition name="image-slide" mode="out-in">
+        <img :key="dynamicStoreImage" :src="dynamicStoreImage" alt="" class="store-img-service" />
+      </transition>
+    </section>
 
-       <section id="store-img-service-select">
-        <div v-for="(image, index) in StoreServices" :key="index" class="service-img-select">
-            <div class="display-image-overlay"></div>
-            <img
-             :src="baseUrl + image.attributes.ServiceImage.data.attributes.url" 
-             alt="" 
-             class="service-img" 
-             @click="updateDynamicImage(index, (baseUrl + image.attributes.ServiceImage.data.attributes.url))"
-             /> 
-        </div>
-       </section>
+    <section id="store-img-service-select">
+      <div v-for="(image, index) in StoreServices" :key="index" class="service-img-select">
+        <div class="display-image-overlay"></div>
+        <img
+          :src="baseUrl + image.attributes.ServiceImage.data.attributes.url"
+          alt=""
+          class="service-img"
+          @click="updateDynamicImage(index, baseUrl + image.attributes.ServiceImage.data.attributes.url)"
+        />
+      </div>
+    </section>
 
-       <section id="customer-related-info">
-        <div class="services-reviews-about-wrapper">
-            <div class="services-reviews-about">
-                <p class="service-heading">Services</p>
-                <!-- <p class="reviews-heading">Reviews</p>
+    <section id="customer-related-info">
+      <div class="services-reviews-about-wrapper">
+        <div class="services-reviews-about">
+          <p class="service-heading">Services</p>
+          <!-- <p class="reviews-heading">Reviews</p>
                 <p class="about-heading">About</p> -->
-            </div>
+        </div>
 
-            <div v-for="(service, index) in StoreServices" :key="index"  class="services-offered">
-                <div class="service-look">
-                    <img :src="baseUrl + service.attributes.ServiceImage.data.attributes.url" alt="" class="service-look-img">
-                </div>
-                <div class="service-details">
-                    <p class="service-name">{{service.attributes.ServiceName}}</p>
-                    <p class="duration">Time: 30 min</p>
-                    <p class="price">Price: {{service.attributes.ServicePrice}}</p>
-                    <p class="more-info-modal"><span id="more-info">More Info</span></p>
-                </div>
+        <div v-for="(service, index) in StoreServices" :key="index" class="services-offered">
+          <div class="service-look">
+            <img :src="baseUrl + service.attributes.ServiceImage.data.attributes.url" alt="" class="service-look-img" />
+          </div>
+          <div class="service-details">
+            <p class="service-name">{{ service.attributes.ServiceName }}</p>
+            <p class="duration">Time: 30 min</p>
+            <p class="price">Price: {{ service.attributes.ServicePrice }}</p>
+            <div class="more-info-modal">
+              <span id="more-info">More Info</span>
+              <button
+                id="more-info-btn"
+                :class="{ infoBtnOn: activeButton === index, infoBtnOff: activeButton !== null && activeButton !== index }"
+                :disabled="activeButton !== null && activeButton !== index"
+                @click="selectService(index)"
+              >
+                +
+              </button>
             </div>
-        </div>
-        <div class="booking-info-wrapper">
-            <div class="booking-info">
-                <div class="booking-btn-wrapper">
-                    <button id="bookAppointment-btn" class="booking-btn-modal" @click.prevent="openModal">Book Now</button>
-                </div>
-                <p class="operating-status"><span id='status'>Closed</span> opens at <span id="operating-status-time"> 09:00</span></p>
-                <a class="location" href="#">{{ location }}</a>
-            </div>
-        </div>
-        <div  v-if="isModalOpen" id="bookAppointmentModal" ref="bookAppointmentModal" @click="closeModal">
-          <div class="select-members-container">
-            <p class="members-heading">SELECT MEMBERS</p>
-            <div class="members">
-              <div class="member" v-for="(member, index) in StoreMembers" @click="getMember(member)">
-                <div class="member-image-wrapper">
-                  <img :src="baseUrl + member.attributes.MemberImage.data.attributes.url" alt="" class="member-img">
-                </div>
-                <p class="member-name">{{ member.attributes.Name }}</p>
-              </div>
-            </div>
-            <button id="bookAppointment-btn" :class="bookingBtn" @click.prevent="bookAppointment" :disabled="isButtonDisabled">Book Now</button>
           </div>
         </div>
-       </section>
-    </main>
+      </div>
+      <div class="booking-info-wrapper">
+        <div class="booking-info">
+          <div class="booking-btn-wrapper">
+            <button id="bookAppointment-btn" class="booking-btn-modal" @click.prevent="openModal">Book Now</button>
+          </div>
+          <p class="operating-status"><span id="status">Closed</span> opens at <span id="operating-status-time"> 09:00</span></p>
+          <a class="location" href="#">{{ location }}</a>
+        </div>
+      </div>
+      <div v-if="isModalOpen" id="bookAppointmentModal" ref="bookAppointmentModal" @click="closeModal">
+        <div class="select-members-container">
+          <p class="members-heading">SELECT MEMBERS</p>
+          <div class="members">
+            <div class="member" v-for="(member, index) in StoreMembers" @click="getMember(member)">
+              <div class="member-image-wrapper">
+                <img :src="baseUrl + member.attributes.MemberImage.data.attributes.url" alt="" class="member-img" />
+              </div>
+              <p class="member-name">{{ member.attributes.Name }}</p>
+            </div>
+          </div>
+          <button id="bookAppointment-btn" :class="bookingBtn" @click.prevent="bookAppointment" :disabled="isButtonDisabled">Book Now</button>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
@@ -161,10 +177,12 @@ const closeModal = (event) => {
 
 .storeName {
   color: #fff;
+  font-size: 1.8rem;
 }
 
 .storeLocation {
   color: #d69c4a;
+  font-size: 1.5rem;
 }
 
 #store-img-service-display {
@@ -200,76 +218,76 @@ const closeModal = (event) => {
   font-size: 1.4rem;
 }
 
-#store-img-service-select{
-    height: 25%;
-    display: flex;
-    margin: 2% 0;
+#store-img-service-select {
+  height: 25%;
+  display: flex;
+  margin: 2% 0;
 }
 
-.service-img-select{
-    position: relative;
-    width: 25%;
-    border: 3px solid #d69c4a;
-    border-radius: 10px;
-    overflow: hidden;
-    margin: 2% 2% 0 0;
+.service-img-select {
+  position: relative;
+  width: 25%;
+  border: 3px solid #d69c4a;
+  border-radius: 10px;
+  overflow: hidden;
+  margin: 2% 2% 0 0;
 }
 
-.service-img-select img{
-    display: block;
-    height: 100%;
-    width: 100%;
-    object-fit: cover;
-    transition: .4s;
+.service-img-select img {
+  display: block;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  transition: 0.4s;
 }
 
-.service-img-select:hover img{
-    transform: scale(1.05);
+.service-img-select:hover img {
+  transform: scale(1.05);
 }
 
-.display-image-overlay{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(255, 255, 255, 0.247);
+.display-image-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.247);
 }
 
-#customer-related-info{
-    display: flex;
-    height: 800px;
-    /* max-height: 710px; */
-    margin-top: 2%;
-    overflow-y: scroll;
+#customer-related-info {
+  display: flex;
+  height: 800px;
+  /* max-height: 710px; */
+  margin-top: 2%;
+  overflow-y: scroll;
 }
 
-.services-reviews-about-wrapper{
+.services-reviews-about-wrapper {
   width: 65%;
   overflow: scroll;
 }
 
-#customer-related-info::-webkit-scrollbar{
+#customer-related-info::-webkit-scrollbar {
   width: 10px;
 }
 
-.services-reviews-about-wrapper::-webkit-scrollbar{
+.services-reviews-about-wrapper::-webkit-scrollbar {
   width: 0;
 }
 
-#customer-related-info::-webkit-scrollbar-thumb, .services-reviews-about-wrapper::-webkit-scrollbar-thumb {
+#customer-related-info::-webkit-scrollbar-thumb,
+.services-reviews-about-wrapper::-webkit-scrollbar-thumb {
   background-color: transparent;
 }
 
-.service-heading{
+.service-heading {
   color: #fff;
   font-weight: bold;
   font-size: 1.45rem;
   text-align: center;
-    
 }
 
-.services-offered{
+.services-offered {
   color: #fff;
   display: flex;
   border-radius: 15px;
@@ -281,114 +299,144 @@ const closeModal = (event) => {
   cursor: pointer;
 }
 
-.service-look{
-    width: 30%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.service-look {
+  width: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 260px;
+  max-height: 192px;
 }
 
 .service-look img {
-    display: block;
-    width: 50%;
-    height: 134px;
-    border-radius: 50%;
-    object-fit: cover;
-    border: 5px solid #d69c4a;
+  display: block;
+  width: 50%;
+  height: 134px;
+  border-radius: 50%;
+  object-fit: cover;
+  border: 5px solid #d69c4a;
 }
 
-.service-details{
-    width: 70%;
-    color: white;
-    border-top-right-radius: 15px;
-    border-bottom-right-radius: 15px;
-    padding: 1%;
-    font-family: initial;
-    line-height: 2;
+.service-details {
+  width: 70%;
+  color: white;
+  border-top-right-radius: 15px;
+  border-bottom-right-radius: 15px;
+  padding: 1%;
+  font-family: initial;
+  line-height: 2;
 }
 
-.service-details p{
-    margin: 0 0 1%;
+.service-details p {
+  margin: 0 0 1%;
 }
 
-.service-name{
+.service-name {
   font-weight: 700;
   font-size: 1.4rem;
   border-bottom: 4px double;
   color: #000;
 }
 
-#more-info{
-    display: inline-block;
-    background: #000;
-    padding: 0.7%;
-    border-radius: 3px;
-    color: #fff;
-    font-weight: 400;
-    font-size: 0.84rem;
+#more-info {
+  display: inline-block;
+  background: #000;
+  padding: 0.7%;
+  border-radius: 3px;
+  color: #fff;
+  font-weight: 400;
+  font-size: 0.84rem;
+  border: none;
 }
 
-.duration, .price, .more-info-modal{
-    font-size: 1.03rem;
+#more-info-btn {
+  background: #000;
+  padding: 0.7%;
+  border-radius: 3px;
+  color: #fff;
+  font-weight: 400;
+  font-size: 0.84rem;
+  text-align: center;
+  border: none;
+  width: 4%;
 }
 
-.booking-info-wrapper{
-    width: 35%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.more-info-modal {
+  display: flex;
+  justify-content: space-between;
 }
 
-.booking-btn-wrapper{
-    display: flex;
-    justify-content: center;
-    margin: 3% 0;
-    height: 20%;
+.infoBtnOn {
+  background: black;
 }
 
-.booking-btn-modal{
-    height: 60%;
-    width: 90%;
-    background: #d69c4a;
-    color: #fff;
-    font-size: 1.2rem;
-    font-weight: 600;
-    border: none;
-    border-radius: 10px;
+.infoBtnOff {
+  background-color: red !important;
 }
 
-.booking-info{
-    width: 70%;
-    height: 70%;
-    background: #fff;
-    border-radius: 10px;
+.duration,
+.price,
+.more-info-modal {
+  font-size: 1.03rem;
 }
 
-.operating-status{
-    text-align: center;
-    font-weight: 600;
-    font-size: 1.05rem;
+.booking-info-wrapper {
+  width: 35%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-#bookAppointmentModal{
-    width: 100vw;
-    height: 100vh;
-    background: #00000078;
-    position: fixed;
-    top: 0;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.booking-btn-wrapper {
+  display: flex;
+  justify-content: center;
+  margin: 3% 0;
+  height: 20%;
 }
 
-.select-members-container{
-    width: 800px;
-    height: 600px;
-    background: #ffff;
-    border-radius: 25px;
-    overflow-y: scroll;
-    position: relative;
+.booking-btn-modal {
+  height: 60%;
+  width: 90%;
+  background: #d69c4a;
+  color: #fff;
+  font-size: 1.2rem;
+  font-weight: 600;
+  border: none;
+  border-radius: 10px;
+}
+
+.booking-info {
+  width: 70%;
+  height: 70%;
+  background: #fff;
+  border-radius: 10px;
+}
+
+.operating-status {
+  text-align: center;
+  font-weight: 600;
+  font-size: 1.05rem;
+}
+
+#bookAppointmentModal {
+  width: 100vw;
+  height: 100vh;
+  background: #00000078;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.select-members-container {
+  width: 800px;
+  height: 600px;
+  background: #ffff;
+  border-radius: 25px;
+  overflow-y: scroll;
+  position: relative;
 }
 
 .select-members-container::-webkit-scrollbar {
@@ -399,22 +447,22 @@ const closeModal = (event) => {
   background-color: transparent; /* Change the color of the scrollbar thumb */
 }
 
-.members-heading{
-    font-weight: 450;
-    font-size: 1.3rem;
-    text-align: center;
+.members-heading {
+  font-weight: 450;
+  font-size: 1.3rem;
+  text-align: center;
 }
 
-.members{
+.members {
   display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(100px, 260px));
-    grid-auto-rows: 40%;
-    gap: 3%;
-    padding-top: 1%;
-    height: 90%;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 260px));
+  grid-auto-rows: 40%;
+  gap: 3%;
+  padding-top: 1%;
+  height: 90%;
 }
 
-.member{
+.member {
   width: 80%;
   height: 80%;
   max-width: 170px;
@@ -423,7 +471,7 @@ const closeModal = (event) => {
   margin: 10% 2% 0% 2%;
 }
 
-.member-image-wrapper{
+.member-image-wrapper {
   height: 100%;
   overflow: hidden;
   display: flex;
@@ -432,30 +480,30 @@ const closeModal = (event) => {
   border-radius: 50%;
   /* transition: .4s; */
 }
-.member-img{
+.member-img {
   display: block;
   width: 100%;
   height: 100%;
   object-fit: cover;
   border-radius: 50%;
-  transition: .4s;
+  transition: 0.4s;
 }
 
-.member-image-wrapper:hover .member-img{
+.member-image-wrapper:hover .member-img {
   height: 140%;
   width: 140%;
 }
 
-.member-image-wrapper:hover{
+.member-image-wrapper:hover {
   border: 5px solid #d69c4a;
 }
 
-.member-name{
+.member-name {
   text-align: center;
   font-weight: 450;
 }
 
-.booking-btn-off{
+.booking-btn-off {
   position: absolute;
   left: 50%;
   bottom: 0;
@@ -472,7 +520,7 @@ const closeModal = (event) => {
   border-radius: 10px;
 }
 
-.booking-btn-on{
+.booking-btn-on {
   position: absolute;
   left: 50%;
   bottom: 0;
