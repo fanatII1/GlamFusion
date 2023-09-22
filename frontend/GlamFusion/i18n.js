72edb38ec64b5e6dbbf9c695075bac9b56161250
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2023-09-22 09:11:04 +0200

    registered all translation files

--- frontend/GlamFusion/i18n.js
+++ frontend/GlamFusion/i18n.js
@@ -3,11 +3,18 @@ import { createI18n } from 'vue-i18n';
 // Import the JSON files for translations
 import en from './locales/en.json';
 import fr from './locales/fr.json';
+import es from './locales/es.json';
+import afr from './locales/afr.json';
+import zu from './locales/zu.json';
+import so from './locales/so.json';
 
 const messages = {
   'en-US': en, // English
   'fr-FR': fr, // French
-//   'es-ES': es, // Spanish
+  'es-ES': es, // Spanish
+  'afr-AFR': afr, // Afrikaans
+  'zu-ZU': zu, // Zulu
+  'so-SO': so // Sesotho
 };
 
 const i18n = createI18n({
